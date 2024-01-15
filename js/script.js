8f895223b5fb4a7826224d2206bd0391be08b603
sisdream <sissipascale2@gmail.com>
2024-01-15 15:53:25 +0100

    feat: js

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// creiamo la variabile
+
+const thumbCont = document.querySelector("thumbs");
+const itemsCont = document.querySelector("items");
+let items = "";
+let thumbs ="";
+
+const images = [
+    "img/01.jpg",
+    "img/02.jpg",
+    "img/03.jpg",
+    "img/04.jpg",
+    "img/05.jpg"
+];
+
+
+
+
+
+
+
+
+
